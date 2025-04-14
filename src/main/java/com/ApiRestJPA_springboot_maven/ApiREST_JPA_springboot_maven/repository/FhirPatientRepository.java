0c61b3912d1f8bb989a91d1c17d3601cece2f4f4
fernando-java-chile <fernando.java.chile@gmail.com>
2025-04-14 18:51:34 -0400

    Agregar métodos personalizados si es necesario

--- src/main/java/com/ApiRestJPA_springboot_maven/ApiREST_JPA_springboot_maven/repository/FhirPatientRepository.java
+++ src/main/java/com/ApiRestJPA_springboot_maven/ApiREST_JPA_springboot_maven/repository/FhirPatientRepository.java
@@ -6,5 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FhirPatientRepository extends JpaRepository<FhirPacient, Integer> {
+    // Aquí puedes agregar métodos personalizados si es necesario
+    // Por ejemplo, para buscar pacientes por nombre, apellido, etc.
+    // List<FhirPacient> findByName(String name);
 
 }
